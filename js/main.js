0e93a572f0493487bf984cbc92b38ded18ab2bf8
Osei-Bonsu Isaac <oseinana.i75@gmail.com>
2022-10-11 08:22:06 +0000

    Fix eslint error

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
-const btn = document.querySelector('#btn'),
-    toggler = document.querySelector('.nav-toggle'),
-    menu = document.querySelector('.nav'),
-    hamburger = document.querySelector('.hamburger'),
-    revLogo = document.querySelector('.rev-logo'),
-    navLink = document.querySelectorAll('.nav-link');
+const btn = document.querySelector('#btn');
+const toggler = document.querySelector('.nav-toggle');
+const menu = document.querySelector('.nav');
+const revLogo = document.querySelector('.rev-logo');
+const navLink = document.querySelectorAll('.nav-link');
 
 btn.addEventListener('click', () => {
-    toggler.classList.toggle('active');
-    menu.classList.toggle('open')
-    revLogo.classList.toggle('active')
-})
+  toggler.classList.toggle('active');
+  menu.classList.toggle('open');
+  revLogo.classList.toggle('active');
+});
 
-navLink.forEach(btnLink => {
-    btnLink.addEventListener('click', () => {
-        toggler.classList.remove('active')
-        menu.classList.remove('open')
-        revLogo.classList.remove('active')
-    })
-})
+navLink.forEach((btnLink) => {
+  btnLink.addEventListener('click', () => {
+    toggler.classList.remove('active');
+    menu.classList.remove('open');
+    revLogo.classList.remove('active');
+  });
+});
